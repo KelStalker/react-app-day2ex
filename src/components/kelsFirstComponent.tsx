fdaefaec0d88e44fb06ff5310b0177f2aea36353
Kel <kelstalker@hotmail.com>
2020-04-08 17:50:33 -0600

    Unable to get semantic components working (with Bo) after over an hour - This is best effort

--- src/components/kelsFirstComponent.tsx
+++ src/components/kelsFirstComponent.tsx
@@ -1,3 +1,9 @@
 import React from "react";
 import {Button, Icon} from 'semantic-ui-react';
 
+export class KelsFirstComponent extends React.Component {
+    render()
+    {
+      return <h1>Kels Component</h1>;
+    }
+  }
